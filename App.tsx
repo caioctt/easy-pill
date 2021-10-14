34bb39f9f1d5839355092ac506bd50915aaa54b0
Caio Tavares <32718663+caioctt@users.noreply.github.com>
2021-10-14 19:21:11 -0300

    adicionados componentes novos

--- App.tsx
+++ App.tsx
@@ -4,10 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Login } from './src/pages/Login';
 import { Welcome } from './src/pages/Welcome';
 
-import Routes from './src/routes'
+import Routes from './src/routes';
 
 export default function App() {
   return (
     // <Welcome></Welcome>
-    <Routes/>
-  )};
+    <Routes />
+  );
+}
